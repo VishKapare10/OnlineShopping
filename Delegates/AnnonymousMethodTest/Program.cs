e11f17171a9e2202bbade29612e47ebbb2f485fc
Vishwambhar Kapare <abhay10kapare@gmail.com>
2021-07-22 01:15:29 +0530

    Implemented LINQ, Annonymous type, Annonymous method and Lambda expression.

---
+++ Delegates/AnnonymousMethodTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AnnonymousMethodTest
+{
+    public delegate void BasicOperation();
+    public delegate void DoSomething();
+    public delegate int annonymousOperation1(int i);
+    public delegate int annonymousOperation2(int a,int b);
+
+    class Program
+    {
+        static void PrintReport(){
+            Console.WriteLine("Printing a report: graph, table, score card");
+        }
+        static void Main(string[] args)
+        {
+            //registering callback function 
+            BasicOperation method1=new BasicOperation(PrintReport);
+            method1();
+
+            //Inline function
+            //Annonymous method
+            BasicOperation method2 = delegate(){
+                Console.WriteLine("Priting company annual report");
+            };
+            method2();
+
+            //Lambda Expression
+            DoSomething method3=()=>{Console.WriteLine("Printing covid spread lockdown report");};
+            method3();
+
+            annonymousOperation1 proxy1=new annonymousOperation1(
+                delegate(int x){
+                    return x*2;
+                }
+            );
+            Console.WriteLine("{0}",proxy1(5));
+
+            annonymousOperation1 proxy2=x=>x*2;
+            Console.WriteLine("{0}",proxy2(25));
+
+
+            annonymousOperation2 getBigInteger= (x,y) => {if (x >y) return x; else return y;};
+            Console.WriteLine("Big integer:"+getBigInteger(10,20));
+
+        }
+    } 
+}
---
+++ Delegates/LINQDemoTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LINQDemoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program theProgram = new Program();
+            Console.WriteLine("Demo for reflection");
+            Type theType1 = theProgram.GetType(); //reflection
+            Console.WriteLine("Type of theProgram variable:"+theType1.Name);
+
+            var instance = new {Id=34,
+                                firstName="Vishwambhar",
+                                lastName="Kapare",
+                                emailAddress="vish10kapare@gmail.com",
+                                contactNumber="7720037983",
+                                Location=new{
+                                    LandMark="Near Karvande Hospital",
+                                    City="Newasa",
+                                    State="Maharashtra",
+                                    Country="India"
+                                }
+                                };
+
+            Type theType2 = instance.GetType();
+            Console.WriteLine("Type of the theType2 is:"+theType2.Name);
+
+            Console.WriteLine("Demo for annonymous data type");
+            Console.WriteLine(instance.firstName+" "+instance.lastName);
+            Console.WriteLine(instance.Location.Country);
+            Console.WriteLine(instance.Location.State);
+            Console.WriteLine(instance.Location.City);
+            Console.WriteLine(instance.Location.LandMark);
+        }
+    }
+}

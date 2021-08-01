919d61946ad167a28db41dfca079e47152817a1e
Vishwambhar Kapare <abhay10kapare@gmail.com>
2021-08-01 16:27:37 +0530

    Implemented basic mvc application.

---
+++ TransflowerWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TransflowerWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

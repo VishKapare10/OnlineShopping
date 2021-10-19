7d6870343b4a0279f9cc188576bf6f06e2dfe4dd
Vishwambhar Kapare <vish10kapare@gmail.com>
2021-10-19 23:46:36 +0530

    Added modular structure.

--- React/basicreactapp/src/App.js
+++ React/basicreactapp/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import Home from './home';
-import List from './List';
-import Dashboard from './Dashboard';
-import Flower from './flower';
-import Aboutus from './aboutus';
-import Contactus  from './contactus';
+import List from './ProductCatalog/List';
+import Dashboard from './ProductCatalog/Dashboard';
+//import Flower from './ProductCatalog/flower';
+import Aboutus from './CRM/aboutus';
+import Contactus  from './CRM/contactus';
+import Login  from './Membership/Login';
 
 
 function App() {
@@ -17,7 +18,8 @@ function App() {
         <li>This is Component based Approch for defining UI</li>
       </ol>
       <hr/>
-      <Home></Home>
+      <Login></Login>
+      {/* <Home></Home>
       <hr/>
       <Dashboard></Dashboard>
       <hr/>
@@ -26,7 +28,7 @@ function App() {
       <hr/>
       <Aboutus></Aboutus>
       <hr/>
-      <Contactus></Contactus>
+      <Contactus></Contactus> */}
     </div>
   );
 }
---
+++ React/basicreactapp/src/ProductCatalog/Product.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import Counter from './Counter';
+
+class Product  extends React.Component{
+
+constructor(props){
+    super(props);
+    this.state={likes:props.likes};
+    this.handler=this.handler.bind(this); //bind handler
+}
+ handler(data){
+     console.log("Parent function is invoked...");
+     console.log(data);
+     this.setState({likes:data});
+ }
+ render(){
+    return <div>
+                <p>Title: {this.props.title}</p>
+                <img src= {this.props.imageurl} width="100" height="100"/>
+                <p>Description:{this.props.description}</p>
+                <p>Unit Price:{this.props.unitprice}</p>
+                <p>Quantity:{this.props.quantity}</p>
+                <p>Likes:{this.state.likes}</p>
+                <br/>
+                <button>Add to Cart</button>
+                <Counter count={this.props.likes} handler={this.handler}></Counter>
+            </div>
+ }
+}
+
+
+export default Product;

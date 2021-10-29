c8ff8beb4486feb4a205836c2162cba939801138
Vishwambhar Kapare <vish10kapare@gmail.com>
2021-10-29 22:52:26 +0530

    Corrected contact us component.

--- React/onlineshoppingapplication/src/App.js
+++ React/onlineshoppingapplication/src/App.js
@@ -96,7 +96,7 @@ function Contact(){
         <p>Address:</p>
         <p>Transflower Learning Pvt. Ltd.</p>
         <p>Walvekar nagar, Pune Satara Road, Pune</p>
-        <p>Email: ravi.tambdade@transflower.in</p>
+        <p>Email: ravi.tambade@transflower.in</p>
         <p>Contact Number: +91-9980771213</p>
     </div>
 );
